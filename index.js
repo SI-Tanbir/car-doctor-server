719c0f0888d464b66ac698ae6121285fac76b5e5
SI-tanbir <si017490@gmail.com>
2024-12-15 14:10:25 +0600

    adding mongo db

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 // Replace the placeholder with your Atlas connection string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9sii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 ;
+     
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri,  {
